0c5a875cf970b8ae7ac26f4898bc9c76ffa65391
Jaxon Poentia <observer@Jaxons-MacBook-Pro.local>
2023-10-14 17:07:41 -0500

    calibration passed

--- lib/camera_pose/pose_detector.dart
+++ lib/camera_pose/pose_detector.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'dart:async';
 import 'dart:math';
 
 import 'package:camera/camera.dart';
@@ -21,12 +22,13 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
       PoseDetector(options: PoseDetectorOptions());
   bool _canProcess = true;
   bool _isBusy = false;
+  bool _firstImage = true;
   CustomPaint? _customPaint;
   String? _text;
 
   bool _isCalibrating = true;
 
-  double maxLength = 0.0;
+  double maxDist = 0.0;
   double rangeError = 0.2;
 
   String _displayText = "cock";
@@ -61,27 +63,65 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
     ]);
   }
 
-  // double getDist(Pose _pose) {
-  //   double sqrtD = 0.0;
-  //   final landmark0 = _pose.landmarks[11]!;
-  //   final landmark1 = _pose.landmarks[12]!;
-  //   final landmark2 = _pose.landmarks[23]!;
-  //   final landmark3 = _pose.landmarks[24]!;
+  double getDist(Pose _pose) {
+    double distL = -1.0;
 
-  //   double avgXDist = pow((landmark0.x - landmark2.x), 2)+pow((landmark1.x - landmark3.x), 2);
-  //   double avgYDist = pow((landmark0.x - landmark1.x), 2).toDouble();
-  //   double avgYDist = pow((landmark1.x - landmark1.x), 2).toDouble();
+    double distR = -1.0;
+    final landmark0 = _pose.landmarks[PoseLandmarkType.leftShoulder];
+    final landmark1 = _pose.landmarks[PoseLandmarkType.rightShoulder];
+    final landmark2 = _pose.landmarks[PoseLandmarkType.leftHip];
+    final landmark3 = _pose.landmarks[PoseLandmarkType.rightHip];
 
-  //   return 0.5;
-  // }
+    if (landmark0?.x != null && landmark2?.x != null) {
+      landmark0!;
+      landmark2!;
+      distL = sqrt(pow((landmark0.x - landmark2.x), 2) +
+          pow((landmark0.y - landmark2.y), 2) +
+          pow((landmark0.z - landmark2.z), 2));
+    }
 
-  // void calibrate(List<Pose> poses) {
-  //   //double dist = poses[];
-  // }
+    if (landmark1?.x != null && landmark3?.x != null) {
+      landmark1!;
+      landmark3!;
+      distR = sqrt(pow((landmark1.x - landmark3.x), 2) +
+          pow((landmark1.y - landmark3.y), 2) +
+          pow((landmark1.z - landmark3.z), 2));
+    }
+    print(distR);
+    print(distL);
+    if (distR < 0 && distL < 0) {
+      return -1;
+    } else {
+      if (distR < 0) {
+        return distL;
+      } else if (distL < 0) {
+        return distR;
+      } else {
+        return (distL + distR) / 2;
+      }
+    }
+  }
+
+  void calibrate(List<Pose> poses) {
+    double tempDist = getDist(poses[0]);
+
+    if (tempDist > maxDist) {
+      setState(() {
+        maxDist = tempDist;
+      });
+    }
+  }
 
   Future<void> _processImage(InputImage inputImage) async {
     if (!_canProcess) return;
     if (_isBusy) return;
+    if (_firstImage) {
+      _firstImage = false;
+      _isCalibrating = true;
+      Timer(const Duration(seconds: 10), () {
+        _isCalibrating = false;
+      });
+    }
     _isBusy = true;
     setState(() {
       _text = '';
@@ -89,13 +129,19 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
     //gets vertexs
     final poses = await _poseDetector.processImage(inputImage);
 
-    if (poses.isNotEmpty) {
-      setState(() {
-        _displayText = poses.isNotEmpty &&
-                poses[0].landmarks[PoseLandmarkType.nose]?.x != null
-            ? poses[0].landmarks[PoseLandmarkType.nose]!.x.toString()
-            : "";
-      });
+    if (!_firstImage) {
+      if (_isCalibrating) {
+        if (poses.isNotEmpty) {
+          calibrate(List.from(poses));
+          setState(() {
+            _displayText = "calibrating, max length is: $maxDist";
+          });
+        }
+      } else {
+        setState(() {
+          _displayText = "done calibrating!";
+        });
+      }
     }
 
     if (inputImage.metadata?.size != null &&
